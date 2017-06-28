3ccce2bb4cf39e5b1c6a8499f3fe44b47b60f56d
RKPutra <putra.bossun@gmail.com>
2017-06-28 22:38:11 +0700

    Update changeBackground.user.js

--- changeBackground.user.js
+++ changeBackground.user.js
@@ -1,4 +1,10 @@
-;(function(){
+// ==UserScript==
+// @name        changeBackground
+// @namespace   aa
+// @include     http://novelstranslation.blogspot.com/p/*
+// @version     1
+// @grant       none
+// ==/UserScript==
 
 document.body.style.color = "#000000";
 var x = document.getElementsByClassName("content-inner");
@@ -6,5 +12,3 @@ var i;
 for (i = 0; i < x.length; i++) {
   x[i].style.backgroundColor = "white";
 }
-  
-  });
